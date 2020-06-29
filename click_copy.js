0c88a6d8feceaf35b2c0ec575273f5570b722ed8
nakagawa331 <67546319+nakagawa331@users.noreply.github.com>
2020-06-29 20:29:01 +0900

    Update click_copy.js

--- click_copy.js
+++ click_copy.js
@@ -1,5 +1,11 @@
 function click_copy(){
     const get_url = document.getElementById("exampleFormControlTextarea1");
-    get_url.select();
-    document.execCommand("Copy");
+    const get_url_len = get_url.value.length;
+    get_url.setSelectionRange(0,get_url_len);
+    const bool = document.execCommand("Copy");
+    if(bool){
+        alert("urlのコピーに成功しました!");
+    } else {
+        alert("コピーに失敗しました");
+    }
 }
